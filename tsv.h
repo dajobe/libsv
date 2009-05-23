ec03c7822efc61bb5e3106ff7f48c532a6904035
Dave Beckett <dave@dajobe.org>
2009-05-22 19:11:35 -0700

    remove unused FH

--- tsv.h
+++ tsv.h
@@ -3,7 +3,7 @@ typedef struct tsv_s tsv;
 typedef int (*tsv_fields_callback)(tsv *t, void *user_data, char** fields, size_t *widths, size_t count);
 
   
-tsv* tsv_init(FILE* fh, void *user_data, tsv_fields_callback callback);
+tsv* tsv_init(void *user_data, tsv_fields_callback callback);
 void tsv_free(tsv *t);
 
 int tsv_get_line(tsv *t);
